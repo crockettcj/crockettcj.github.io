3ea9fe606af9277373869784a98177b3251028bf
Catherine Crockett <crockettcj@gmail.com>
2016-01-11 21:22:29 -0500

    added resume link and updated contact form

--- js/scripts.js
+++ js/scripts.js
@@ -17,7 +17,7 @@ jQuery(document).ready(function() {
         var postdata = $('.contact-form form').serialize();
         $.ajax({
             type: 'POST',
-            url: 'assets/contact.php',
+            url: '/../assets/contact.php',
             data: postdata,
             dataType: 'json',
             success: function(json) {
